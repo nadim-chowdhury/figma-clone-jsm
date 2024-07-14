4360a4d4680e093730d09722b1210dff33dadc12
nadim-chowdhury <nadim-chowdhury@outlook.com>
2024-07-14 21:48:11 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { CollaborativeApp } from "./CollaborativeApp";
+import { Room } from "./Room";
+
+export default function Home() {
+  return (
+    <Room>
+      <CollaborativeApp />
+    </Room>
+  );
+}
